68c04387e050b99727aec27576426a524955e2ec
RuanRoah <1278457484@qq.com>
2017-07-18 23:17:10 +0800

    first commit

---
+++ routes/dao.js
@@ -0,0 +1,23 @@
+/**
+ * Created by asrh0 on 2017/4/27.
+ */
+//db.js
+// 连接MySQL
+var mysql = require('mysql');
+var pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'nodedb'
+});
+
+function query(sql, callback) {
+    pool.getConnection(function (err, connection) {
+        // Use the connection
+        connection.query(sql, function (err, rows) {
+            callback(err, rows);
+            connection.release();//释放链接
+        });
+    });
+}
+exports.query = query;
